811b29b2ee63c6c2d7e10e5da790f108a170dad8
lalomolino <lalomolino@gmail.com>
2024-12-05 13:07:14 -0500

    Settings page added

--- src/app/pages/setting/setting/setting.page.ts
+++ src/app/pages/setting/setting/setting.page.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import {CommonModule, DatePipe} from "@angular/common";
 import { FormsModule } from "@angular/forms";
-import { IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonHeader, IonTitle, IonGrid, IonRow, IonCol, IonContent, IonCard, IonIcon, IonToolbar, IonButtons, IonMenuButton } from "@ionic/angular/standalone";
+import {
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonHeader,
+  IonTitle,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonContent,
+  IonCard,
+  IonIcon,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonInput, IonItem, IonButton, IonDatetime, IonText, IonLabel, IonPopover
+} from "@ionic/angular/standalone";
 
 @Component({
   selector: "app-setting",
   templateUrl: "./setting.page.html",
   styleUrls: ["./setting.page.scss"],
   standalone: true,
-  imports: [IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonHeader, IonTitle, IonGrid, IonRow, IonCol, IonContent, IonCard, IonIcon, IonToolbar, IonButtons, IonMenuButton],
+  imports: [IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonHeader, IonTitle, IonGrid, IonRow, IonCol, IonContent, IonCard, IonIcon, IonToolbar, IonButtons, IonMenuButton, IonInput, IonItem, IonButton, IonDatetime, IonText, IonLabel, IonPopover, FormsModule, DatePipe],
 })
 export class SettingPage implements OnInit {
+  date: any;
   constructor() {}
 
   ngOnInit() {}
