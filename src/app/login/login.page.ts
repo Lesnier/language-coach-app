29e68997080f48f5d9b39227e57b7ffe41a31701
lalomolino <lalomolino@gmail.com>
2024-12-06 19:04:36 -0500

    Added redirection from login until auth is implemented

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,19 +2,19 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-
+import {Router, RouterLink} from "@angular/router";
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, ReactiveFormsModule],
+  imports: [IonicModule, CommonModule, ReactiveFormsModule, RouterLink],
 })
 export class LoginPage {
   loginForm: FormGroup;
   showLayout: any;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private router: Router) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
@@ -22,9 +22,10 @@ export class LoginPage {
   }
 
   onLogin() {
-    if (this.loginForm.valid) {
+    /*if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
       console.log('Login con:', email, password);
-    }
+    }*/
+    this.router.navigate(['/start']);
   }
 }
