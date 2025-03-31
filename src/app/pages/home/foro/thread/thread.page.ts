ab6db9148b601806054b9e9f16b16b8b8365099e
Alejandro Pérez Durán <apduran95@gmail.com>
2025-03-31 10:54:43 -0400

    feat: Update thread page to display title dynamically and handle empty comments

--- src/app/pages/home/foro/thread/thread.page.ts
+++ src/app/pages/home/foro/thread/thread.page.ts
@@ -1,41 +1,26 @@
-import { Component, DoCheck, inject, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import {
+  IonButton,
+  IonButtons,
   IonContent,
+  IonFooter,
   IonHeader,
+  IonIcon,
+  IonItem,
+  IonMenuButton,
+  IonTextarea,
   IonTitle,
   IonToolbar,
-  IonButtons,
-  IonMenuButton,
-  IonAccordion,
-  IonAccordionGroup,
-  IonItem,
-  IonLabel,
-  IonAvatar,
-  IonCard,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
-  IonCol,
-  IonGrid,
-  IonIcon,
-  IonRow,
-  IonButton,
-  IonInput,
-  IonTextarea,
-  IonText,
-  IonCardContent,
-  IonList,
-  IonFooter,
 } from '@ionic/angular/standalone';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ApiService } from 'src/app/services/api.service';
-import { chevronBackOutline, send } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
-import { UtilsService } from 'src/app/services/utils.service';
+import { chevronBackOutline, send } from 'ionicons/icons';
 import { User } from 'src/app/models/interfaces';
+import { ApiService } from 'src/app/services/api.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-thread',
@@ -105,19 +90,43 @@ export class ThreadPage implements OnInit {
       this.api.getThreads(token).subscribe((res) => {
         res.forEach((element: any) => {
           if (this.id == element.id) {
+            this.title = element.comment; // Set the title from thread comment
             this.comments = element.threadreplys;
 
             for (let i = 0; i < this.comments.length; i++) {
-              this.comments[i].response = JSON.parse(this.comments[i].response)
-              
+              try {
+                // Try to parse the response as JSON
+                this.comments[i].response = JSON.parse(
+                  this.comments[i].response
+                );
+              } catch (e) {
+                // If parsing fails, create a compatible object with the text
+                this.comments[i].response = {
+                  name: this.getUserNameById(this.comments[i].user_id),
+                  text: this.comments[i].response,
+                };
+              }
             }
             console.log(this.comments);
-            
-            // setTimeout(() => {
-            //   this.chatContent.scrollToBottom(200);
-            // }, 0);
+
+            setTimeout(() => {
+              this.chatContent.scrollToBottom(200);
+            }, 200);
           }
         });
       });
+  }
+
+  // Helper method to get user name by ID
+  getUserNameById(userId: number): string {
+    // If users are available, find the user
+    const user = this.users.find((u) => u.id === userId);
+    if (user) return user.name;
+
+    // Fallback to current user if ID matches
+    if (this.user.id === userId) return this.user.name;
+
+    // Default fallback
+    return 'Usuario';
   }
 }
