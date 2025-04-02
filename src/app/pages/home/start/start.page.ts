2641ba0dfa5c4b1403e99ff3edb56d1dd1348c55
Alejandro Pérez Durán <apduran95@gmail.com>
2025-04-02 11:45:38 -0400

    feat: Remove unused student profile images and schedule test components

--- src/app/pages/home/start/start.page.ts
+++ src/app/pages/home/start/start.page.ts
@@ -1,45 +1,102 @@
-import { Component, inject, OnInit } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import {
-    IonCardHeader,
-    IonCardTitle,
-    IonHeader,
-    IonTitle,
-    IonGrid,
-    IonRow,
-    IonCol,
-    IonContent,
-    IonCard,
-    IonIcon,
-    IonToolbar,
-    IonButtons,
-    IonButton,
-    IonMenuButton,
-    IonRouterLink,
-    IonItemDivider, IonItemGroup, IonLabel,
-} from "@ionic/angular/standalone";
-import { addIcons } from "ionicons";
-import { calendarOutline, calendarSharp, layersSharp, layersOutline, chatbubbleSharp, checkboxSharp, chevronBackOutline } from "ionicons/icons";
-
+  IonButton,
+  IonButtons,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonItemDivider,
+  IonItemGroup,
+  IonLabel,
+  IonMenuButton,
+  IonRouterLink,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  calendarOutline,
+  calendarSharp,
+  chatbubbleSharp,
+  checkboxSharp,
+  layersOutline,
+  layersSharp,
+} from 'ionicons/icons';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
-  selector: "app-start",
-  templateUrl: "./start.page.html",
-  styleUrls: ["./start.page.scss"],
+  selector: 'app-start',
+  templateUrl: './start.page.html',
+  styleUrls: ['./start.page.scss'],
   standalone: true,
-    imports: [IonIcon, IonCardHeader, IonCardTitle, IonCard, IonCol, IonRow, IonGrid, IonContent, IonHeader, IonTitle, IonButton, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonRouterLink, RouterLink, RouterLinkActive, IonItemDivider, IonItemGroup, IonLabel],
+  imports: [
+    IonIcon,
+    IonCardHeader,
+    IonCardTitle,
+    IonCard,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonButton,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonButtons,
+    IonMenuButton,
+    IonRouterLink,
+    RouterLink,
+    RouterLinkActive,
+    IonItemDivider,
+    IonItemGroup,
+    IonLabel,
+  ],
 })
 export class StartPage implements OnInit {
+  outstandingStudents: any[] = [];
+  api = inject(ApiService);
 
-  constructor() {
-    addIcons({ calendarOutline, calendarSharp, layersSharp, layersOutline, chatbubbleSharp, checkboxSharp });
+  constructor(private http: HttpClient) {
+    addIcons({
+      calendarOutline,
+      calendarSharp,
+      layersSharp,
+      layersOutline,
+      chatbubbleSharp,
+      checkboxSharp,
+    });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchOutstandingStudents();
+  }
 
-
-
-
+  fetchOutstandingStudents() {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      this.api.getStudents(token).subscribe((response: any) => {
+        if (response && response.Students) {
+          this.outstandingStudents = response.Students.filter(
+            (student: { is_outstanding: boolean }) => student.is_outstanding
+          ).map((student: any) => ({
+            ...student,
+            profile_picture_url:
+              student.profile_picture_url || 'assets/default-profile.png', // Fallback image
+          }));
+        }
+      });
+    }
+  }
 }
